9e5ffa1eb94e9fa33ce591d92d1e721be4abd220
Florian Dubath <florian.dubath@unige.ch>
2019-04-23 13:29:27 +0200

    Mount the page and add the logic (to be validated)

--- app/includes/note.tpl
+++ app/includes/note.tpl
@@ -21,13 +21,113 @@
                    <div class="col-md-12 ">
                      <div class="row "> 
                          <div class="col-md-12 ">
-                             <label translate="NOT_Title" >Total positions changes</label>
+                             <label translate="NOT_Title" >Adjust Bank note amount</label>
                            
                          </div>
                      </div>  
                  </div> 
-              </div>  
+                 
+                 <div class="col-md-12 " ng_hide="processing">
+                     <div class="row "> 
+                         <div class="col-md-12 ">
+                              <input style="display:none;" type="file" on-read-file="openListFile($fileContent)" id="ntflstselector"/>
+                              
+                              <button type="button" class="btn btn-info" ng-click="openGetList()"  translate="NOT_Btn_getList" > Get List </button>     
+                          </div>
+                          <div class="col-md-12 ">
+                            &nbsp;
+                          </div>
+                     </div>  
+                 </div>  
+                 <div class="col-md-12 ">
+                     <div class="row "> 
+                         <div class="col-md-12 ">
+                              <label translate="NOT_amount" >Charge with:</label>
+                              
+                              <select ng-options="opt for opt in value_options" data-ng-model="target_amount_option" ng-readonly="processing"></select>
+                          </div>
+                          <div class="col-md-12 ">
+                            &nbsp;
+                          </div>
+                     </div>  
+                 </div>       
+                <div class="col-md-12 " ng_hide="processing || processed">
+                     <div class="row "> 
+                         <div class="col-md-12 ">
+                              <button type="button" class="btn btn-info" ng-click="run()"  translate="NOT_Btn_charge"  >Charge </button>     
+                         </div>
+                          <div class="col-md-12 ">
+                            &nbsp;
+                          </div>
+                     </div>  
+                 </div> 
+                 <div class="col-md-12 " ng_show="processing">
+                     <div class="row "> 
+                         <div class="col-md-12">
+                              <label translate="NOT_progress" >Progress:</label>{{done}}/{{total}}
+                         </div>
+                     </div>  
+                 </div>
+                 <div class="col-md-12 " ng_show="processed">
+                     <div class="row "> 
+                         <div class="col-md-12">
+                              <label translate="NOT_completed" >completed</label>
+                         </div>
+                     </div>  
+                 </div> 
+                 <div class="col-md-12 ">
+                    <div class="row "> 
+                      <div class="col-md-12 ">
+                        <table width="100%">
+   
+                            <tr ng-show="showNone" class="tr_trans">
+                               <td colspan="3">
+                                  <p translate="NOT_NoAdd" >Nones</p>
+                               </td>
+	                        </tr>
+	                        <tr  ng-repeat="curr_address in info_list track by $index" class="tr_trans">
+                             <td >
+                                {{curr_address.address}}
+                             </td>
+                             <td>
+                                {{curr_address.amount | number : 2}} {{CUR}}
+                             </td>
+                             <td>
+                                {{curr_address.status | translate}}
+                             </td>
+	                        </tr>
+                       </table>
+                   </div>
+                 </div>  
+               </div>  
+            </div>  
          </div> 
       </section>
+      
+      
+      
+      
+        <!-- Popup confirm run -->
+      <div class="modal fade" id="pop_conf_run" tabindex="-1" role="dialog" aria-labelledby="sendTransactionLabel">
+              <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      </div>
+                      <div class="modal-body">
+                          <div align="center">
+                           <h4><label translate="NOT_ConfirmTitle">Confirm run</label></h4>
+                            <label translate="NOT_confirm_text">Charging with</label>
+                            {{target_amount | number : 2}}{{CUR}}
+                          </div>
+                      </div>
+                      <div class="modal-footer text-center">
+                          <button type="button" class="btn btn-default" data-dismiss="modal" translate="NOT_Cancel">Annuler </button>
+                          <button type="button" class="btn btn-primary" ng-click="doRun()" translate="NOT_btn_conf_run">Remove</button>
+                      </div>
+                  </div>
+              </div>
+         </div>
+      
     </div>
 </div> 
--- app/scripts/controllers/noteCtrl.js
+++ app/scripts/controllers/noteCtrl.js
@@ -4,9 +4,20 @@ var noteCtrl = function($scope, $locale, $sce, walletService, $translate) {
     $scope.isApp =  globalFuncs.isApp();
     $scope.currentWalletAddress=globalFuncs.getWalletAddress();
     $scope.CUR='';
-    $scope.CUR_nanti='';
-    $scope.CUR_credit_mut='';
-    $scope.has_nant=false;
+    
+    $scope.showNone = true;
+    $scope.processing = false;
+    $scope.processed = false;
+    $scope.target_amount = 0.00;
+    $scope.total = 0;
+    $scope.done = 0;
+    $scope.info_list = [];
+    $scope.value_options =[];
+    $scope.target_amount_option = null;
+     
+    // Popups
+	$scope.conf_popup = new Modal(document.getElementById('pop_conf_run'));
+    
     
     
     $scope.$watch(function() {
@@ -24,14 +35,195 @@ var noteCtrl = function($scope, $locale, $sce, walletService, $translate) {
             });
             
             $scope.CUR=globalFuncs.currencies.CUR;
-            $scope.CUR_nanti=globalFuncs.currencies.CUR_nanti;
-            $scope.CUR_credit_mut=globalFuncs.currencies.CUR_credit_mut;
             
-            $scope.has_nant=globalFuncs.hasNant();
+            $scope.value_options = globalFuncs.getNoteValues();
+            $scope.value_options.unshift(0.00);
+            $scope.target_amount_option = $scope.value_options[0];
             
             globalFuncs.hideLoadingWaiting();  
         });
     
+  $scope.openGetList = function() {
+        document.getElementById('ntflstselector').click();
+  }
+  
+  
+  $scope.openListFile = function($fileContent) {
+      if (document.getElementById('ntflstselector').files[0]){
+        try {
+            var new_list = $fileContent.match(/0x[a-f0-9]{40}/mgi);
+            $scope.showNone = new_list.length == 0;
+            $scope.prepareList(new_list);
+        } catch (e) {
+          alert($translate.instant("NOT_NoValidAddress"));    
+        }
+	  }
+  }
+  
+  $scope.preparInfo = function(address, length){
+        globalFuncs.getAccInfo(globalFuncs.slockitAccStatus, address, function(status){
+             globalFuncs.getAmmount(globalFuncs.slockitElBlance, address, function(value){
+                var status_txt = "NOT_Locked";
+                if (status == 1){
+                    status_txt = "NOT_Unlocked";
+                }
+                var info = {"address":address, "amount":value, "st":status, "status":status_txt};
+                $scope.local_list.push(info);
+                
+                if ($scope.local_list.length == length){
+                     $scope.info_list = $scope.local_list;
+                }
+             });
+          });
+  }
+  
+  $scope.prepareList = function(address_list) {
+      $scope.info_list = [];
+      $scope.local_list = [];
+      $scope.processed = false;
+      for (var index in address_list){
+        $scope.preparInfo(address_list[index], address_list.length);
+       
+      }
+  }
+  
+  
+  
+  $scope.run = function() {
+      $scope.target_amount = $scope.target_amount_option;
+      $scope.conf_popup.open();
+  }
+  
+  $scope.doRun = function(){
+      $scope.conf_popup.close();
+      $scope.processing = true;
+      $scope.total = 0;
+      $scope.done = 0;
+      for (var index in $scope.info_list){
+          var info = $scope.info_list[index];
+          if (info.amount == $scope.target_amount){
+              if (info.st == 1){ 
+                 $scope.total+=1; // right amount but not locked
+              } else {
+                  // right amount and locked: nothing to do
+              }
+          } else {
+              if (info.st == 1){ 
+                 $scope.total+=2; // wrong amount but not locked
+              } else {
+                 $scope.total+=3; // wrong amount and locked
+              }
+          }
+      }
+      
+      $scope.curr_index = 0;
+      $scope.processInfo();
+ 
+  }
+  
+  $scope.unlock = function(address){
+      globalFuncs.SetAccountParam($scope.wallet, address, 1, 0, 0, 0, function(data){
+                                         if (data.isError){
+                                            alert($translate.instant("NOT_Processing_error") + data.error);
+                                         } else {
+                                            $scope.waitTransaction(data.data); 
+                                         }
+                                    });   
+    }
+  
+  $scope.lock = function(address){
+       globalFuncs.SetAccountParam($scope.wallet, address, 0, 0, 0, 0, function(data){
+                                         if (data.isError){
+                                            alert($translate.instant("NOT_Processing_error") + data.error);
+                                         } else {
+                                            $scope.waitTransaction(data.data); 
+                                         }
+                                    });
+  }
+  
+  $scope.adjustAmount = function(address, amount){
+       globalFuncs.PledgeAccount($scope.wallet, address, amount, function(data){
+                                         if (data.isError){
+                                             alert($translate.instant("NOT_Processing_error") + data.error);
+                                         } else {
+                                             $scope.waitTransaction(data.data);
+                                         }
+                                   });
+  }
+  
+  // Processing coordinator
+  $scope.processInfo = function(){
+      if ($scope.curr_index == $scope.info_list.length){
+           $scope.completed();
+      } else {
+          var address = $scope.info_list[$scope.curr_index].address;
+          globalFuncs.getAccInfo(globalFuncs.slockitAccStatus, address, function(status){
+             globalFuncs.getAmmount(globalFuncs.slockitElBlance, address, function(value){
+                 $scope.info_list[$scope.curr_index].amount = value;
+                 var status_txt = "NOT_Locked";
+                 if (status == 1){
+                    status_txt = "NOT_Unlocked";
+                 }
+                 $scope.info_list[$scope.curr_index].st = status;
+                 $scope.info_list[$scope.curr_index].status = status_txt;
+                 
+                 if (value == $scope.target_amount){
+                      if (status == 1){ 
+                          // right amount but not locked
+                          $scope.lock(address);
+                      } else {
+                          // right amount and locked
+                          $scope.curr_index += 1;
+                          $scope.processInfo();
+                      }
+                 } else {
+                      if (status == 1){ 
+                          // wrong amount but not locked
+                          $scope.adjustAmount(address, value - $scope.target_amount);
+                      } else {
+                          // wrong amount and locked
+                          $scope.unlock(address);
+                      }
+                 }
+                 
+             });
+          });  
+      }
+  }
+
+  // way out
+  $scope.completed = function() {
+     $scope.processing = false;
+     $scope.processed = true;
+  }
+  
+  
+  $scope.interval_id=null;
+  
+  $scope.recievedTransaction = function(){
+        clearInterval($scope.interval_id);
+        $scope.done += 1;
+        $scope.processInfo();
+        $scope.refresh();
+        $scope.$apply();
+  }
+  
+  $scope.waitTransaction = function(transaction_ash){
+      if ($scope.interval_id){
+          clearInterval($scope.interval_id);
+          $scope.interval_id=null;
+      }
+      
+      $scope.interval_id = setInterval(function(){
+          ajaxReq.getBlock(transaction_ash, function(block_json){
+              if (block_json.blockNumber && block_json.blockNumber.startsWith('0x')){
+                 $scope.recievedTransaction();
+              }
+          });
+      },5000);  
+  }  
+
+  
   
    
 }
