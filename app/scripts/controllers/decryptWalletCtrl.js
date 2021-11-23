26514b6640471ec32fa50893ba7d1f1419a8792c
Valentin Lab <valentin.lab@kalysto.org>
2021-11-23 19:15:23 +0100

    fix: ``jsc3l.message.ensureWalletMessageKey()`` change to ``wallet.ensureWalletMessageKey()`` new API
    Signed-off-by: Valentin Lab <valentin.lab@kalysto.org>

--- app/scripts/controllers/decryptWalletCtrl.js
+++ app/scripts/controllers/decryptWalletCtrl.js
@@ -276,12 +276,12 @@ var decryptWalletCtrl = function($scope, $sce, $translate, walletService, contac
             walletService.password = $scope.filePassword;
             walletService.wallet = $scope.wallet;
             
-            
-          jsc3l.message.ensureWalletMessageKey($scope.wallet, $translate.instant('WALL_missing_message_key')).then(function(complete_wall) {
-            $scope.wallet = complete_wall;
-            walletService.wallet = $scope.wallet;
-            
-            localStorage.setItem('ComChainWallet',JSON.stringify(jsc3l.wallet.encryptWallet($scope.wallet, $scope.filePassword)));
+          $scope.wallet.ensureWalletMessageKey().then(function(complete_wall) {
+            if (typeof complete_wall === 'string') {
+               // TODO: need to remove alerts
+              alert($translate.instant('WALL_missing_message_key'));
+            }
+            localStorage.setItem('ComChainWallet',JSON.stringify($scope.wallet.encryptWallet($scope.filePassword)));
 
             globalFuncs.loadWallets(true);
           });
