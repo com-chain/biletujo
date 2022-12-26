5181a50dc5bad8e979e0961c8694d9d86bb94ee3
Florian Dubath <florian.dubath@unige.ch>
2022-12-26 09:09:57 +0100

    remove hack hiding JSC3L bug

--- app/scripts/directives/blockiesDrtv.js
+++ app/scripts/directives/blockiesDrtv.js
@@ -5,14 +5,10 @@ var blockiesDrtv = function() {
         scope.$watch(watchVar, function() {
              const address = attrs.blockieAddress;
              if(jsc3l.ethFuncs.validateEtherAddress(address)){
-                   element.css({'background-image': 'url(' + globalFuncs.getBlockie(address) +')' });
+                 element.css({'background-image': 'url(' + globalFuncs.getBlockie(address) +')' });
              } else {
                  const img_add =attrs.img==1 ? 'images/lem.png' : 'images/qrclick.png';
-                 const base_url = jsc3l.customization.getCurrencyAssetBaseUrl();
-                 // Protect against early load (when the customization is not ready) by falling back on the local CSS
-                 if (!base_url.startsWith('undefined')) {
-                    element.css({'background-image': 'url(' + base_url +'/' + img_add +')' });
-                 }
+                 element.css({'background-image': 'url(' + jsc3l.customization.getCurrencyAssetBaseUrl() +'/' + img_add +')' });
              }
         });
     };
