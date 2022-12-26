3818f3e91bb914f8a0bba3149c5add4c34860ad6
Florian Dubath <florian.dubath@unige.ch>
2022-12-26 08:56:45 +0100

    Fix refreshing of the visual cryptoram

--- app/scripts/directives/blockiesDrtv.js
+++ app/scripts/directives/blockiesDrtv.js
@@ -3,17 +3,17 @@ var blockiesDrtv = function() {
 	return function(scope, element, attrs){
 	   var watchVar = attrs.watchVar;
         scope.$watch(watchVar, function() {
-            var address = attrs.blockieAddress;
-            var img_id = attrs.img;
-            
-             var img_add =img_id==1 ? 'images/lem.png' : 'images/qrclick.png';
-             
-             element.css({'background-image': 'url(${jsc3l.customization.getCurrencyAssetBaseUrl()}/' + img_add +')' });
-                
+             const address = attrs.blockieAddress;
              if(jsc3l.ethFuncs.validateEtherAddress(address)){
                    element.css({'background-image': 'url(' + globalFuncs.getBlockie(address) +')' });
+             } else {
+                 const img_add =attrs.img==1 ? 'images/lem.png' : 'images/qrclick.png';
+                 const base_url = jsc3l.customization.getCurrencyAssetBaseUrl();
+                 // Protect against early load (when the customization is not ready) by falling back on the local CSS
+                 if (!base_url.startsWith('undefined')) {
+                    element.css({'background-image': 'url(' + base_url +'/' + img_add +')' });
+                 }
              }
-           
         });
     };
 };
