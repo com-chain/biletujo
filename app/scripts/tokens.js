1aeb486239226eb09cd6dbbaf543d8ffd85ba1cd
Valentin Lab <valentin.lab@kalysto.org>
2023-05-17 12:23:27 +0200

    fix: catch exception when using Token

--- app/scripts/tokens.js
+++ app/scripts/tokens.js
@@ -4,8 +4,8 @@ var Token = function(contractAddress, userAddress, symbol, decimal) {
 	this.userAddress = userAddress
 	this.symbol = symbol;;
 	this.decimal = decimal;
-	this.setBalance();
     this.balance = "loading";
+	this.setBalance();
 }
 Token.balanceHex = "0x70a08231";
 Token.transferHex = "0xa9059cbb";
@@ -28,12 +28,17 @@ Token.prototype.getBalance = function() {
 Token.prototype.getBalanceBN = function() {
 	return this.balanceBN;
 }
-Token.prototype.setBalance = function() {
+Token.prototype.setBalance = async function() {
 	var balanceCall = jsc3l.ethFuncs.getDataObj(this.contractAddress, Token.balanceHex, [jsc3l.ethFuncs.getNakedAddress(this.userAddress)]);
 	var parentObj = this;
-    jsc3l.ajaxReq.getEthCall(balanceCall).then(function(data) {
+    let data
+    try {
+      data = await jsc3l.ajaxReq.getEthCall(balanceCall)
+    } catch(err) {
+      console.log("Ignored Exception", err)
+      return;
+    }
 			parentObj.balance = new BigNumber(data).div(new BigNumber(10).pow(parentObj.getDecimal())).toString();
 			parentObj.balanceBN = new BigNumber(data).toString();
-    });
 }
 module.exports = Token;
